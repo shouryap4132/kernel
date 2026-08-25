8e56489b2160866ff9eeca9a8896b2ad0988d4d9
Shourya <adroit.shourya@gmail.com>
2026-08-25 11:38:49 -0700

    memorry addresses

---
+++ kernel/Memory Address.md
@@ -0,0 +1,4 @@
+One character takes 1 memory address
+A integer takes 4 memory address
+
+A variable' address is always the address of its first byte.
--- pointers.c
+++ pointers.c
@@ -2,15 +2,9 @@
 // studio in / out
 
 int main() {
-    int num = 20;
-    // &num holds the memory address of num
+    char *str = "hello world";
 
-    int *ptr = &num;
-
-    // now printing 'ptr' will actually print the address, however when you want the actual variable stored at that address you have to reference to it as *ptr
-
-    printf("%d\n", *ptr); // this will print out 20
-
-    return 0;
+    printf("%p\n", (void*)str);
+    printf("Address of str: %p\n", (void*)&str);
 }
 
